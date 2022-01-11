32823ce42c6f8371c23f7117f4331722eb8eb667
Ian Foster <mrlanrat@gmail.com>
2022-01-11 11:42:57 -0800

    variables and coutners for clearing

--- run.sh
+++ run.sh
@@ -11,24 +11,35 @@ filename="hass.png"
 kindle="192.168.250.2"
 image_url="http://geary:5000/3.png"
 fbink="/mnt/us/fpink/K3/bin/fbink"
+rotate=false
 
 ssh_flags="-q -o ConnectTimeout=1 -o ServerAliveInterval=1 -o StrictHostKeyChecking=no"
+
+refresh_every=10
+sleep_duration=5m
+count=0
 
 update_screen() {
     echo "> Downloading"
     curl --silent "$image_url" -o "$filename"
 
-    #convert image.png -rotate 90
-    # in place rotation
-    #echo "> Rotationg"
-    #mogrify -rotate -90 "$filename"
+    if [ "$rotate" == true ]; then
+        #convert image.png -rotate 90
+        # in place rotation
+        echo "> Rotating"
+        mogrify -rotate -90 "$filename"
+    fi
 
     echo "> Transfering"
     sshpass -p '' scp $ssh_flags "$filename" root@$kindle:"/mnt/us/$filename"
 
-    echo "> Clearing Screen"
-    sshpass -p '' ssh $ssh_flags "root@$kindle" "$fbink" --quiet -hk
-    #sshpass -p '' ssh $ssh_flags "root@$kindle" "$fbink" --quiet -c #--invert 'a'
+    # clear screen every n refreshes
+    if ! (($count % $refresh_every)); then
+        echo "> Clearing Screen"
+        sshpass -p '' ssh $ssh_flags "root@$kindle" "$fbink" --quiet -hk
+        #sshpass -p '' ssh $ssh_flags "root@$kindle" "$fbink" --quiet -c #--invert 'a'
+    fi
+
     echo "> Rendering Image"
     #sshpass -p '' ssh $ssh_flags "root@$kindle" "$fbink" --quiet --invert -g file="/mnt/us/$filename",w=-1
     sshpass -p '' ssh $ssh_flags "root@$kindle" "$fbink" --quiet -g file="/mnt/us/$filename",w=-1
@@ -39,8 +50,10 @@ update_screen() {
 
 while true
 do
-    echo "> loop"
+    echo "> loop $count"
     update_screen
-    sleep 5m
+    let "count+=1"
+    echo ""
+    sleep "$sleep_duration"
 done
 
