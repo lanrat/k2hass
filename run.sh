a8517b7068d7d65f2987d97b3aff745fbe72a2ca
Ian Foster <mrlanrat@gmail.com>
2022-01-18 09:59:49 -0800

    cleanup

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
+name="k2hass"
 
-docker stop k2hass
-docker rm k2hass
+echo "removing old container $name"
+docker stop "$name"
+docker rm "$name"
 
+echo "starring new $name"
 docker run \
     --restart=unless-stopped \
-    --name k2hass \
+    --name "$name" \
+    -d \
     -it \
     -e kindle_ip="192.168.250.2" \
     -e image_url="http://192.168.1.4:5000/3.png" \
     -e fbink="/mnt/us/fpink/K3/bin/fbink" \
     lanrat/k2hass
 
+
+docker logs --follow "$name"
